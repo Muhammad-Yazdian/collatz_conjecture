6cfbca11137a6357df0992e6d639b8179ed637de
muhammad-yazdian <muhammad.yazdian@gmail.com>
2021-11-04 21:37:23 +0100

    Other rules diverge

--- main.c
+++ main.c
@@ -19,27 +19,33 @@
 #include<stdio.h>
 
 int main(void){
-  // Get user input
+  // Test for range of initial values
+  unsigned int range_upper_limit = 1;
   unsigned int num = 0;
   unsigned int iterations = 0;
-  printf("Collatz Conjecture\n");
-  printf("Enter an positive integer number: ");
-  scanf("%d", &num);
-  printf("Iteration #%d: %d\n", iterations, num);
+  
+  printf("Similar to Collatz Conjecture\n");
+  printf("Enter the upper limit of a range [1, n]: ");
+  scanf("%d", &range_upper_limit);
+  
+  FILE* file_out;
+  file_out = fopen("iteration_by_other.txt", "w");
 
-  // 2- If the number is odd  => n = 3n+1 elseif even => n = n/2
-  // 3- Repeate step #2 until you are in a 4-2-1 loop
-  while(num !=1){
-    if(num % 2 == 0){
-      num /= 2;
-    }else{
-      num = 3 * num + 1;
+  int i = 1;
+  for(i; i<=range_upper_limit; i++){
+    num = i;
+    iterations = 0;
+    while(iterations < 50){
+      fprintf(file_out, "%d, ", num);
+      // Apply rules other than Collatz conjecture one
+      if(num % 2 == 0){
+        num /= 2;
+      }else{
+        num = 5 * num + 3y;
+      }
+      iterations++;
     }
-    iterations++;
-    printf("Iteration #%d: %d\n", iterations, num);
+    fprintf(file_out, "\n");
   }
-    
-  // 4- Find the number of iterations to get from intial n to 1
-    printf("Totoal number of iterations = %d\n", iterations);
   return 0;
 }
