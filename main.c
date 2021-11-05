fd32c812f404b07fb39ac0a4586775d13d03e56b
muhammad-yazdian <muhammad.yazdian@gmail.com>
2021-11-05 19:53:16 +0100

    Sample temp file created.

--- main.c
+++ main.c
@@ -18,25 +18,39 @@ int main(void){
   unsigned int num = 1;
   unsigned int num_a = 1;
   unsigned int num_b = 1;
+  unsigned int num_left = 0;
+  unsigned int num_right = 0;
+  char line[256];
+  unsigned int line_counter = 1;
+  int n;
+  int offset = 0;
   FILE* file;
-  file = fopen("temp.txt", "r");
+  file = fopen("temp.txt", "a+");
   if (!file){
     fprintf(stderr, "Error: Could not find the file!\n");
     exit(-1);
   }
   //fseek(file, 0, SEEK_SET );
-  while (fscanf(file, "%d, ", &num) != EOF){
-    printf("Reading ... %d\n", num);
+  //while (fscanf(file, "(%d, %d), ", &num_left, &num_right) != EOF){
+  //while (end_of_file = fgets(file, "(%d, %d), ", &num_left, &num_right) != EOF){
+  while(fgets(line, sizeof(line), file))
+  {
+    line_counter++; // Do nothing. This gives us the last line.
   }
-  num_a = num;
-  num_b = num;
+  printf("The last line is %s\n", line);
+  printf("Getting data from last line (%d) ... ", line_counter);
   fprintf(file, "\n");
-  if ((num_a - 1) % 3 == 0 && num_a !=1){
-    num_a = (num_a - 1) / 3;
-    fprintf(file, "%d, ", num_a);
+  while(sscanf(line + offset, " (%d, %d),%n", &num_left, &num_right, &n) == 2){
+    offset += n;
+    printf("(%d, %d), ", num_left, num_right);
+    fprintf(file, "(%d, %d), ", num_left+1, 2 * num_left);
+    // if ((num_a - 1) % 3 == 0 && num_a !=1){
+    //   num_a = (num_a - 1) / 3;
+    //   fprintf(file, "%d, ", num_a);
+    // }
   }
-  num_b = num_b * 2;
-  fprintf(file, "%d, ", num_b);
+  offset = 0;
+  n = 0;
   fclose(file);
   return 0;
 }
